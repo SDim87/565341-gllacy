f9da4a06b7ac97b1cbf782bd4069c97b5c6b1325
SDim87 <fatalmixx@yandex.ru>
2018-04-06 21:47:13 +0300

    Готов к 2й проверке

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,9 @@
 var link = document.querySelector(".contacts__btn ");
 var popup = document.querySelector(".modal-feedback");
 var overlay = document.querySelector(".modal-overlay");
+var switchers = document.querySelectorAll(".slider__inner");
+var body = document.querySelector("body");
+var modalClose = document.querySelector(".modal__close");
 
 link.addEventListener("click", function(evt){
   evt.preventDefault();
@@ -8,3 +11,21 @@ link.addEventListener("click", function(evt){
   overlay.classList.add("modal-overlay--show");
 });
 
+modalClose.addEventListener("click", function(evt){
+  evt.preventDefault();
+  popup.classList.remove("modal-feedback--show");
+  overlay.classList.remove("modal-overlay--show");
+});
+
+for(var i = 0; i < switchers.length; i++){
+  switchers[i].addEventListener("click", function(evt){
+    evt.preventDefault();
+    for(var k = 0; k < switchers.length; k++){
+      switchers[k].classList.remove("slider__inner--active");
+    };
+    this.classList.add("slider__inner--active");
+    body.className = "";
+    var id = this.dataset.id;
+    body.classList.add("bg-icecream-" + id);
+  });
+};
