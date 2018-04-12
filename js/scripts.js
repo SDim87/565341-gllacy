014f7fb11b92fed0d7f87b56e2743a8555acaa68
SDim87 <fatalmixx@yandex.ru>
2018-04-12 18:20:33 +0300

    Последние правки

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,9 @@ var overlay = document.querySelector(".modal-overlay");
 var switchers = document.querySelectorAll(".slider__inner");
 var body = document.querySelector("body");
 var modalClose = document.querySelector(".modal__close");
+var form = popup.querySelector(".modal-form");
+var firstName = popup.querySelector("[name=first-name]");
+var email = popup.querySelector(".modal-form__email");
 
 link.addEventListener("click", function(evt){
   evt.preventDefault();
@@ -15,8 +18,29 @@ modalClose.addEventListener("click", function(evt){
   evt.preventDefault();
   popup.classList.remove("modal-feedback--show");
   overlay.classList.remove("modal-overlay--show");
+  popup.classList.remove("modal-feedback--error");
+});
+form.addEventListener("submit", function (evt) {
+  if (!firstName.value || !email.value) {
+    evt.preventDefault();
+    popup.classList.remove("modal-feedback--error");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("modal-feedback--error");
+  }
+});
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (popup.classList.contains("modal-feedback--show")) {
+      popup.classList.remove("modal-feedback--show");
+      overlay.classList.remove("modal-overlay--show");
+      popup.classList.remove("modal-feedback--error");
+    }
+  }
 });
 
+
+/*===== slider ===*/
 for(var i = 0; i < switchers.length; i++){
   switchers[i].addEventListener("click", function(evt){
     evt.preventDefault();
@@ -29,3 +53,6 @@ for(var i = 0; i < switchers.length; i++){
     body.classList.add("bg-icecream-" + id);
   });
 };
+
+
+
